7959b020a4ac5fad5f3846d4bf2d46b9b1548404
Mike Wilkerson <mwilkerson@gmail.com>
2018-05-29 16:32:42 -0700

    Add rollup configuration

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,14 @@
 // rollup.config.js
 import resolve from 'rollup-plugin-node-resolve'
 import { uglify } from 'rollup-plugin-uglify'
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 
 export default {
   input: 'src/index.js',
   plugins: [
     resolve(),
+    commonjs(),
     uglify()
   ],
   output: {
