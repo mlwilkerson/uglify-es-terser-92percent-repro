3a6cee13d85522c173f3fc77bffee0fcd1752809
Mike Wilkerson <mwilkerson@gmail.com>
2018-05-29 16:57:05 -0700

    remove extra resolve import

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 // rollup.config.js
 import resolve from 'rollup-plugin-node-resolve'
 import { uglify } from 'rollup-plugin-uglify'
-import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 
 export default {
