8a0ecbd91bf809ded7a0525d102e62b812c2487f
Mike Wilkerson <mwilkerson@gmail.com>
2018-06-05 17:12:48 -0700

    Demonstrate workaround for deep imports with webpack 4.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { library, dom } from '@fortawesome/fontawesome-svg-core'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
-import { faBell } from '@fortawesome/free-regular-svg-icons'
-import { faFontAwesome } from '@fortawesome/free-brands-svg-icons'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons/faCoffee'
+import { faBell } from '@fortawesome/free-regular-svg-icons/faBell'
+import { faFontAwesome } from '@fortawesome/free-brands-svg-icons/faFontAwesome'
 library.add(faCoffee, faBell, faFontAwesome)
 dom.watch()
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,8 @@
 const path = require('path')
-const UglifyWebpackPlugin = require('uglifyjs-webpack-plugin')
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle-webpack.js'
-  },
-  optimization: {
-    minimizer: [
-      new UglifyWebpackPlugin({
-        uglifyOptions: {
-          compress: {
-            collapse_vars: false
-          }
-        }
-      })
-    ]
   }
 }
